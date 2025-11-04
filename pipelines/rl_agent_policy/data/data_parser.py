fee5c0b6eeecf794d3dce47180a05f8bb47841f4
Artem Fedorov <s02210577@gse.cs.msu.ru>
2025-11-04 18:16:03 +0300

    fix of error

--- pipelines/rl_agent_policy/data/data_parser.py
+++ pipelines/rl_agent_policy/data/data_parser.py
@@ -185,7 +185,7 @@ class DataHandler:
                 data = data[['date', 'open', 'high', 'low', 'close', 'volume']].copy()
                 data = self._prepare_raw_data(data)
                 
-                data = data.rename(columns={'date': 'timestamp', 'open': 'open_price', 'high': 'high_price', 'low': 'low_price', 'close': 'close_price', 'volume': 'volume_price'})
+                # data = data.rename(columns={'date': 'timestamp', 'open': 'open_price', 'high': 'high_price', 'low': 'low_price', 'close': 'close_price', 'volume': 'volume_price'})
                 
                 raw_data[currency] = data
                 logger.info(f"✓ Successfully fetched {len(data)} records for {currency}")
